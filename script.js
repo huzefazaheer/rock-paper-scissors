2d08c9604495e0292931d29af5daa81510045348
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-05 01:04:31 +0500

    Added computers selection function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+console.log("Javascript loaded!");
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random(3));
+  console.log(choice);
+
+  switch (choice) {
+    case 0:
+      console.log("The computer choose Rock");
+      break;
+    case 1:
+      console.log("The computer choose Paper");
+      break;
+    case 2:
+      console.log("The computer choose Scissors");
+      break;
+    default:
+      console.log("An unexpected error has occured");
+      break;
+  }
+  return choice;
+}
+
+getComputerChoice();
